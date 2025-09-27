07034fd6d1589fd7918dfd7020abe0e2506b12e2
Md Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2025-09-27 13:02:21 +0600

    config file updated

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -13,3 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export default firebaseConfig;
